0e5127e253b965be4988f833b931313d832822f2
Ghulam Mustafa <mustafaji@gmail.com>
2021-11-19 11:52:29 +0100

    added MoveOneAnother

---
+++ MoveOneAnother/app.js
@@ -0,0 +1,7 @@
+function moveButton(elem) {
+  if ($(elem).parent().attr("id") == "nonSelected") {
+    $(elem).detach().appendTo("#selected");
+  } else {
+    $(elem).detach().appendTo("#nonSelected");
+  }
+}
