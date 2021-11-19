3c3a4328bb6a9e1354d82e86740abe39ea2d94d9
Ghulam Mustafa <mustafaji@gmail.com>
2021-11-19 12:33:09 +0100

    added selectJSONValaues

---
+++ selectJSONValues/app.js
@@ -0,0 +1,10 @@
+colors = { color1: "Red", color2: "Green", color3: "Blue" };
+
+$.each(colors, function (key, value) {
+  $("#divSelect").append(
+    $("<option/>", {
+      value: key,
+      text: value,
+    })
+  );
+});
