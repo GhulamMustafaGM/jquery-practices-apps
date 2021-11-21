19fb9684f236ea1a762eb5f9bbc87644d742a43a
Ghulam Mustafa <mustafaji@gmail.com>
2021-11-21 13:59:30 +0100

    added selectedValuesText

---
+++ selectedValuesText/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("#button1").click(function () {
+    console.log($("#op2").val());
+    console.log($("#op4").val());
+    console.log($("#ddid :selected").text());
+  });
+});
